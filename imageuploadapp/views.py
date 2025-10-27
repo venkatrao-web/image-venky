8a7e46e11df41d1e0cda7c86dc98c86e2ef1e9e6
venkat <venkatraopalam@gmail.com>
2025-10-27 09:34:37 +0530

    new addaed

---
+++ imageuploadapp/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from imageuploadapp.forms import ImageUploadForm
+from imageuploadapp.models import ImageUploadModel
+
+# Create your views here.
+def upload_file(request):
+    if request.method=='POST':
+        form=ImageUploadForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("<h2>Image upload successfully..</h2>")
+    else:
+        form=ImageUploadForm()
+        images=ImageUploadModel.objects.all()
+        return render(request,'index.html',{'form':form,'images':images})
+
+
